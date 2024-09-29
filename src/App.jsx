9dd172f186ff8e9cefe8254a4cc6d849d032eb82
fleemarket <questional777@gmail.com>
2024-09-30 00:17:54 +0900

    init: project start

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+                <iframe
+                    src="https://kr.widgets.investing.com/top-cryptocurrencies"
+                    width="500px"
+                    height="300px"
+                    frameBorder="0"
+                    allowtransparency="true"
+                    marginWidth="0"
+                    marginHeight="0"
+                ></iframe>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
+}
+
+export default App;
